b663c420feb70ca5d38ac612f175827b5b346a14
Treelight <43248389@qq.com>
2021-04-08 16:30:33 +0800

    first commit

--- 批改器类.py
+++ 批改器类.py
@@ -1,5 +1,14 @@
+# 与正确的Python文件对比，用于批改学生的Python文件
 class CorrectManager(object):
     def __init__(self):
-        # correct_answer为标准答案，student_answer为学生答案
+        # correct_answer为正确答案，student_answer为学生答案,均为文件路径
         self.correct_answer = ''
         self.student_answer = ''
+
+    def update_correctanswer(self, path):
+        # 更新正确答案路径
+        self.correct_answer = path
+
+    def update_studentanswer(self, path):
+        # 更新学生答案路径
+        self.student_answer = path
