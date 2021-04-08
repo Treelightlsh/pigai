17f83602a3a9eba857fd4235a10b604aa3db8e9d
Treelight <43248389@qq.com>
2021-04-08 15:49:07 +0800

    first commit

--- 批改器类.py
+++ 批改器类.py
@@ -1,3 +1,5 @@
 class CorrectManager(object):
     def __init__(self):
-        pass
+        # correct_answer为标准答案，student_answer为学生答案
+        self.correct_answer = ''
+        self.student_answer = ''
